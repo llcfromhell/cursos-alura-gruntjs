f57aa8a864dd23aa646e66126d686c576bd6b156
Lourenço Lima da Cunha <lourenco.cunha@POA.E-CORE.COM>
2017-06-20 10:00:58 -0300

    aula 02 - 03

--- projeto/Gruntfile.js
+++ projeto/Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-    
+
     grunt.initConfig({
 
         copy: {
@@ -8,21 +8,46 @@ module.exports = function (grunt) {
                 cwd: 'public',
                 src: '**',
                 dest: 'dist'
-            } 
+            }
         },
 
         clean: {
             dist: {
                 stc: 'dist'
             }
+        },
+
+        useminPrepare: {
+            html: 'dist/**/*.html'
+        },
+
+        usemin: {
+            html: 'dist/**/*.html'
+        },
+
+        imagemin: {
+            public: {
+                expand: true,
+                cwd: 'dist/img',
+                src: '**/*.{png,jpg,gif}',
+                dest: 'dist/img'
+            }
         }
 
     })
 
     grunt.registerTask('dist', ['clean', 'copy'])
-    grunt.registerTask('default', ['dist'])
+
+    grunt.registerTask('min', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin', 'imagemin'])
+
+    grunt.registerTask('default', ['dist', 'min'])
 
     grunt.loadNpmTasks('grunt-contrib-copy')
     grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-concat')
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-contrib-cssmin')
+    grunt.loadNpmTasks('grunt-contrib-imagemin')
+    grunt.loadNpmTasks('grunt-usemin')
 
 }
